b0b2bc1efc2ab7209002fb081fef45712c1562bf
Gayle Martin <hello.gmartin@gmail.com>
2023-08-27 20:59:20 -0500

    I need to go to sleep

--- server.js
+++ server.js
@@ -6,15 +6,14 @@ const PORT = 666;
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  return res.sendFile(path.join(__dirname, './src/index.html'));
+    return res.sendFile(path.join(__dirname, './src/index.html'));
 });
 
 app.get('/api', (req, res) => {
-  console.log(res.body);
-  res.json({ posts: ['post1', 'post2', 'post3'] });
+    res.json({ posts: ['post1', 'post2', 'post3'] });
 });
 
 // This does not seem to work
 app.listen(666, () => {
-  console.log('Server started on port 666');
+    console.log('Server started on port 666');
 });
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,32 +3,34 @@ import React, { useEffect, useState } from 'react';
 import { createRoot } from 'react-dom/client';
 
 function App() {
-  const [nasaData, setNasaData] = useState({});
-  const [postData, setPostData] = useState([]);
+    const [nasaData, setNasaData] = useState({});
+    const [postData, setPostData] = useState([]);
 
-  useEffect(() => {
-    // Testing fetching external API -- this works
-    fetch('https://api.nasa.gov/planetary/apod?api_key=4LI6zdP9bsTQ8EtctAniacB3nVF7t2fceEa3SIgF')
-      .then(response => response.json())
-      .then(data => {
-        setNasaData(data);
-      });
+    useEffect(() => {
+        // Testing fetching external API -- this works
+        fetch(
+            'https://api.nasa.gov/planetary/apod?api_key=4LI6zdP9bsTQ8EtctAniacB3nVF7t2fceEa3SIgF'
+        )
+            .then((response) => response.json())
+            .then((data) => {
+                setNasaData(data);
+            });
 
-    // Backend API on server.js -- this does not work
-    fetch('/api')
-      .then(response => response.json())
-      .then(data => {
-        setPostData(data);
-      });
-  }, []);
+        // Backend API on server.js -- this does not work
+        fetch('/api')
+            .then((response) => response.json())
+            .then((data) => {
+                setPostData(data);
+            });
+    }, []);
 
-  return (
-    <div>
-      <p>is anyone home?</p>
-      <p>{nasaData.explanation}</p>
-      <p>{postData.posts}</p>
-    </div>
-  );
+    return (
+        <div>
+            <p>is anyone home?</p>
+            <p>{nasaData.explanation}</p>
+            <p>{postData.posts}</p>
+        </div>
+    );
 }
 
 const root = createRoot(document.querySelector('#root'));
