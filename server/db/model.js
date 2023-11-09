4b045d9464fea098cb4a4a1949c9cfc934a3e556
Gayle Martin <hello.gmartin@gmail.com>
2023-11-09 16:59:48 -0600

    Connect to database

--- server/db/model.js
+++ server/db/model.js
@@ -1,26 +1,24 @@
 const mongoose = require('mongoose');
-
-const MONGO_URI =
-    'mongodb+srv://hellogmartin:YH8RshIUeh6sst2d@cluster0.yhyya61.mongodb.net/?retryWrites=true&w=majority';
+require('dotenv');
 
 mongoose
-    .connect(MONGO_URI, {
-        // options for the connect method to parse the URI
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        // sets the name of the DB that our collections are part of
-        dbName: 'quil_app'
-    })
-    .then(() => console.log('Connected to Mongo DB.'))
-    .catch((err) => console.log(err));
+  .connect(process.env.MONGODB_URI, {
+    // options for the connect method to parse the URI
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // sets the name of the DB that our collections are part of
+    dbName: 'quil_app',
+  })
+  .then(() => console.log('Connected to Mongo DB.'))
+  .catch(err => console.log(err));
 
 const Schema = mongoose.Schema;
 
 const entrySchema = new Schema(
-    {
-        body: { type: String, required: true }
-    },
-    { timestamps: true }
+  {
+    body: { type: String, required: true },
+  },
+  { timestamps: true },
 );
 
 const Entry = mongoose.model('entry', entrySchema);
