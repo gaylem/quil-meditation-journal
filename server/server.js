a519afe899ed954ccf71364c96aa94f8ca3b0c12
Gayle Martin <hello.gmartin@gmail.com>
2023-11-09 15:36:54 -0600

    Remove unnecessary files and clean up server.js

--- server/server.js
+++ server/server.js
@@ -1,15 +1,21 @@
+require('dotenv').config();
+const cookieParser = require('cookie-parser');
 const path = require('path');
 const express = require('express');
 const app = express();
 const apiRouter = require('./api');
 const PORT = 4000;
 
-// Handle parsing request body
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// Cookies
+const cookieController = require('./controllers/cookieController');
 
 // Handle requests for static files
-app.use(express.static(path.resolve(__dirname, '../assets'))); // TODO: should this be client?
+app.use(express.static(join(__dirname, '../assets')));
 
 // Define route handlers
 app.use('/api', apiRouter);
@@ -19,31 +25,31 @@ app.use((req, res) => res.status(404).send("This is not the page you're looking 
 
 // Express error handler
 app.use((err, req, res, next) => {
-    const defaultErr = {
-        log: 'Express error handler caught unknown middleware error',
-        status: 500,
-        message: { err: 'An error occurred' }
-    };
-    const errorObj = Object.assign({}, defaultErr, err);
-    console.log(errorObj.log);
-    return res.status(errorObj.status).json(errorObj.message);
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 500,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
 });
 
 // Environments
 if (process.env.NODE_ENV === 'production') {
-    // statically serve everything in the build folder on the route '/build'
-    app.use('/build', express.static(path.join(__dirname, '../build')));
+  // statically serve everything in the build folder on the route '/build'
+  app.use('/build', express.static(path.join(__dirname, '../build')));
 
-    // serve the index.html on a request to the root
-    // app.get('/', (req, res) => {
-    //     return res.status(200).sendFile(path.join(__dirname, '../index.html'));
-    // });
+  // serve the index.html on a request to the root
+  // app.get('/', (req, res) => {
+  //     return res.status(200).sendFile(path.join(__dirname, '../index.html'));
+  // });
 }
 console.log('NODE_ENV: ', process.env.NODE_ENV);
 
 // Start Server
 app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
+  console.log(`Listening on port ${PORT}`);
 });
 
 module.exports = app;
