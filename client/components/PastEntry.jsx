68e8126cfdad705d7021fe7709c3e776bffbad73
Gayle Martin <hello.gmartin@gmail.com>
2023-11-13 12:50:29 -0600

    Progress on state management strategy

--- client/components/PastEntry.jsx
+++ client/components/PastEntry.jsx
@@ -2,75 +2,62 @@ import React, { useState } from 'react';
 import '../scss/pastEntry.scss';
 import moment from 'moment';
 
-const PastEntry = (props) => {
-    // TOGGLE ENTRY
-    const [open, setOpen] = useState(false);
-    const toggle = () => {
-        setOpen(!open);
-    };
+const PastEntry = ({ entry, id }) => {
+  // TOGGLE ENTRYå
+  const [open, setOpen] = useState(false);
+  const toggle = () => {
+    setOpen(!open);
+  };
 
-    // FORMAT DATE
-    const createdDate = props.date;
-    const formattedDate = moment(createdDate).format('LL');
+  // FORMAT DATE
+  const createdDate = entry.createdAt;
+  const formattedDate = moment(createdDate).format('LL');
 
-    // HANDLE DELETE
-    const handleDelete = (e) => {
-        e.preventDefault();
+  // HANDLE DELETE
+const handleDelete = async e => {
+  e.preventDefault();
 
-        fetch(`entries/${props.id}`, {
-            method: 'DELETE',
-            body: JSON.stringify(props.entry)
-        })
-            .then((res) => {
-                res.json();
-                console.log(res._id);
-            })
-            .catch((error) => console.error('Error:', error));
+  try {
+    const response = await axios.delete(`entries/${id}`);
+    console.log(response.data); // Check the server response
+  } catch (error) {
+    console.error('Error deleting entry:', error);
+  }
 
-        toggle();
-    };
+  toggle();
+};
 
-    // TODO: HANDLE EDIT - Button works when I have postman opened
-    const handleEdit = (e) => {
-        e.preventDefault();
+  // TODO: HANDLE EDIT - Button works when I have postman opened
+  const handleEdit = e => {
+    e.preventDefault();
 
-        fetch(`entries/${props.id}`, {
-            method: 'PATCH'
-        })
-            .then((res) => {
-                res.json();
-            })
-            .catch((error) => console.error('Error:', error));
-    };
+    fetch(`entries/${id}`, {
+      method: 'PATCH',
+    })
+      .then(res => {
+        res.json();
+      })
+      .catch(error => console.error('Error:', error));
+  };
 
-    return (
-        <div className="PastEntry">
-            <div className="buttonHeaderPast">
-                <button onClick={toggle}>+</button>
-                <p>{formattedDate}</p>
+  return (
+    <div key={entry._id} className='PastEntry'>
+      <div className='buttonHeaderPast'>
+        <button onClick={toggle}>+</button>
+        <p>{formattedDate}</p>
+      </div>
+      <div>
+        {open && (
+          <div className='toggle'>
+            <p className='pastEntryText'>{entry.body}</p>
+            <div className='pastEntryButtons'>
+              <input id='cancel' type='submit' onClick={handleDelete} value='Delete'></input>
+              <input id='save' type='submit' onClick={handleEdit} value='Edit'></input>
             </div>
-            <div>
-                {open && (
-                    <div className="toggle">
-                        <p className="pastEntryText">{props.body}</p>
-                        <div className="pastEntryButtons">
-                            <input
-                                id="cancel"
-                                type="submit"
-                                onClick={handleDelete}
-                                value="Delete"
-                            ></input>
-                            <input
-                                id="save"
-                                type="submit"
-                                onClick={handleEdit}
-                                value="Edit"
-                            ></input>
-                        </div>
-                    </div>
-                )}
-            </div>
-        </div>
-    );
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 export default PastEntry;
---
+++ client/hooks/useEntriesContext.jsx
@@ -0,0 +1,12 @@
+import { useContext } from 'react';
+import { EntriesContext } from '../context/EntriesContext';
+
+export const useEntriesContext = () => {
+  const context = useContext(EntriesContext);
+
+  if (!context) {
+    throw new Error('useEntriesContext must be used within an EntriesContextProvider. Make sure your component is wrapped with the EntriesContextProvider.');
+  }
+
+  return context;
+};
