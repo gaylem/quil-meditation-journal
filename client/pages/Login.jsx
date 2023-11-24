1899719f0726f60dee7b3c5782ec33bc6a0f603d
Gayle Martin <hello.gmartin@gmail.com>
2023-11-24 16:36:33 -0600

    Update comments and classNames on Login and Signup pages and stylesheet, and refactor JSX

--- client/pages/Login.jsx
+++ client/pages/Login.jsx
@@ -3,44 +3,68 @@ import { Link } from 'react-router-dom';
 import { useLogin } from '../hooks/useLogin';
 import '../scss/login-signup.scss';
 
+/**
+ * Login component represents the login page of the application.
+ *
+ * It allows users to log in using their username and password.
+ *
+ * @returns {JSX.Element} The rendered Login component.
+ */
 function Login() {
+  // State variables to manage username, password, and login-related state
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const { login, error, isLoading } = useLogin();
 
+  /**
+   * Handles the form submission for user login.
+   *
+   * @param {Event} e - The submit event.
+   */
   const handleSubmit = async e => {
     e.preventDefault();
     await login(username, password);
   };
 
   return (
-    <div className='login-signup-container'>
-      <div className='login-signup'>
-        <div className='box'>
-          <form onSubmit={handleSubmit}>
-            <h1 className='title'>Welcome back!</h1>
-            <input type='username' onChange={e => setUsername(e.target.value)} placeholder='Username' value={username} />
-            <input type='password' onChange={e => setPassword(e.target.value)} placeholder='Password' value={password} />
-            <button className='login-signup-btn' type='submit' disabled={isLoading}>
-              Log in
-            </button>
-            {error && <div className='error'>{error}</div>}
-            <p>
-              Don't have an account? <Link to='/signup'>Sign up!</Link>
-            </p>
-          </form>
+    // Main login and signup container
+    <div className='loginSignupContainer'>
+      {/* Box containing the login form */}
+      <div className='box'>
+        {/* Login form */}
+        <form onSubmit={handleSubmit}>
+          {/* Title */}
+          <h1 className='title'>Welcome back!</h1>
+          {/* Username input */}
+          <input type='username' onChange={e => setUsername(e.target.value)} placeholder='Username' value={username} />
+          {/* Password input */}
+          <input type='password' onChange={e => setPassword(e.target.value)} placeholder='Password' value={password} />
+          {/* Login button */}
+          <button className='loginSignupBtn' type='submit' disabled={isLoading}>
+            Log in
+          </button>
+          {/* Display error message if there is an error */}
+          {error && <div className='error'>{error}</div>}
+          {/* Link to the signup page */}
+          <p>
+            Don&apos;t have an account? <Link to='/signup'>Sign up!</Link>
+          </p>
+        </form>
+      </div>
+      {/* About section */}
+      <div className='about'>
+        {/* First paragraph of about section */}
+        <div className='paragraph'>
+          <p>quil is inspired by the words &quot;tranquil&quot; and &quot;quill,&quot; as in the classic feather pen. Sign up or log in to discover your own dedicated space, where you can chronicle each step of your meditation journey.</p>
         </div>
-        <div className='about'>
-          <div className='paragraph'>
-            <p>quil is inspired by the words "tranquil" and "quill," as in the classic feather pen. Sign up or log in to discover your own dedicated space, where you can chronicle each step of your meditation journey.</p>
-          </div>
-          <div>
-            <p>Every entry you record in your account is treated with the utmost security, employing robust encryption to ensure absolute privacy and confidentiality. Put your mind at ease and your worries aside, and immerse yourself fully in the transformative experience of meditation. We hope we can help your innermost thoughts find a safe harbor amid the turbulant waves of life.</p>
-          </div>
+        {/* Second paragraph of about section */}
+        <div>
+          <p>Every entry you record in your account is treated with the utmost security, employing robust encryption to ensure absolute privacy and confidentiality. Put your mind at ease and your worries aside, and immerse yourself fully in the transformative experience of meditation. We hope we can help your innermost thoughts find a safe harbor amid the turbulent waves of life.</p>
         </div>
       </div>
     </div>
   );
 }
 
+// Export the Login component as the default export
 export default Login;
--- client/pages/Signup.jsx
+++ client/pages/Signup.jsx
@@ -3,63 +3,87 @@ import { Link } from 'react-router-dom';
 import { useSignup } from '../hooks/useSignup';
 import '../../client/scss/login-signup';
 
+/**
+ * Signup component represents the signup page of the application.
+ *
+ * It allows users to create a new account by providing a username, email, and password.
+ *
+ * @returns {JSX.Element} The rendered Signup component.
+ */
 function Signup() {
+  // State variables to manage username, email, password, and signup-related state
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const { signup, error, isLoading } = useSignup();
 
+  /**
+   * Handles the form submission for user signup.
+   *
+   * @param {Event} e - The submit event.
+   */
   const handleSubmit = async e => {
     e.preventDefault();
+    // Call the signup function from the hook to initiate the signup process
     await signup(username, email, password);
   };
 
   return (
-    <div className='login-signup-container'>
-      <div className='login-signup'>
-        <div className='box'>
-          <form onSubmit={handleSubmit}>
-            <h1 className='title'>Tranquility awaits...</h1>
-            <input
-              type='username'
-              onChange={e => {
-                setUsername(e.target.value);
-              }}
-              placeholder='Username'
-              value={username}
-            />
-            <input
-              type='email'
-              onChange={e => {
-                setEmail(e.target.value);
-              }}
-              placeholder='Email'
-              value={email}
-            />
-            <input
-              type='password'
-              onChange={e => {
-                setPassword(e.target.value);
-              }}
-              placeholder='Password'
-              value={password}
-            />
-            <button className='login-signup-btn' disabled={isLoading}>
-              Sign Up
-            </button>
-            {error && <div className='error'>{error}</div>}
-            <p>
-              Already have an account? <Link to='/'>Sign in!</Link>
-            </p>
-          </form>
+    <div className='loginSignupContainer'>
+      {/* Box containing the signup form */}
+      <div className='box'>
+        {/* Signup form */}
+        <form onSubmit={handleSubmit}>
+          {/* Title */}
+          <h1 className='title'>Tranquility awaits...</h1>
+          {/* Username input */}
+          <input
+            type='username'
+            onChange={e => {
+              setUsername(e.target.value);
+            }}
+            placeholder='Username'
+            value={username}
+          />
+          {/* Email input */}
+          <input
+            type='email'
+            onChange={e => {
+              setEmail(e.target.value);
+            }}
+            placeholder='Email'
+            value={email}
+          />
+          {/* Password input */}
+          <input
+            type='password'
+            onChange={e => {
+              setPassword(e.target.value);
+            }}
+            placeholder='Password'
+            value={password}
+          />
+          {/* Signup button */}
+          <button className='loginSignupBtn' disabled={isLoading}>
+            Sign Up
+          </button>
+          {/* Display error message if there is an error */}
+          {error && <div className='error'>{error}</div>}
+          {/* Link to the signin page */}
+          <p>
+            Already have an account? <Link to='/'>Sign in!</Link>
+          </p>
+        </form>
+      </div>
+      {/* About section */}
+      <div className='about'>
+        {/* First paragraph of about section */}
+        <div className='paragraph'>
+          <p>quil is inspired by the words &quot;tranquil&quot; and &quot;quill,&quot; as in the classic feather pen. Sign up or log in to discover your own dedicated space, where you can chronicle each step of your meditation journey.</p>
         </div>
-        <div className='about'>
-          <div className='paragraph'>
-            <p>quil is inspired by the words "tranquil" and "quill," as in the classic feather pen. Sign up or log in to discover your own dedicated space, where you can chronicle each step of your meditation journey.</p>
-          </div>
-          <div>
-            <p>Every entry you record in your account is treated with the utmost security, employing robust encryption to ensure absolute privacy and confidentiality. Put your mind at ease and your worries aside, and immerse yourself fully in the transformative experience of meditation. We hope we can help your innermost thoughts find a safe harbor amid the turbulant waves of life.</p>
-          </div>
+        {/* Second paragraph of about section */}
+        <div>
+          <p>Every entry you record in your account is treated with the utmost security, employing robust encryption to ensure absolute privacy and confidentiality. Put your mind at ease and your worries aside, and immerse yourself fully in the transformative experience of meditation. We hope we can help your innermost thoughts find a safe harbor amid the turbulent waves of life.</p>
         </div>
       </div>
     </div>
