14db4899327a155ff27a5337d3caf15471e1f441
Gayle Martin <hello.gmartin@gmail.com>
2023-11-27 16:25:12 -0600

    Remove middleware from getAllEntries route and update comments

--- server/routers/entryRouter.js
+++ server/routers/entryRouter.js
@@ -4,56 +4,42 @@ const router = express.Router();
 // Entry controllers
 const { getAllEntries, addEntry, findEntry, updateEntry, deleteEntry } = require('../controllers/entryController');
 
-// Verify access token controller
-const { verifyAccessToken } = require('../utils/token.utils');
-
 /**
  * @route GET /entries
- * @description Get all entries
- * @access Private (requires access token)
+ * @description Get all entries for a specific user
+ * @param req.params userId: String
  */
- // TODO: Update get requests to send userId in req.params
-router.get('/', verifyAccessToken, getAllEntries);
+router.get('/:id', getAllEntries);
 
+// TODO: findEntry might not be needed
 /**
  * @route GET /entries/:id
  * @description Get a specific entry by ID
- * @param req.params
- *  - entry ID: String
- * @access // TODO: make Private (requires access token)
+ * @param req.params entry ID: String
  */
-// TODO: Add verifyAccessToken
 router.get('/:id', findEntry);
 
 /**
  * @route DELETE /entries/:id
  * @description Delete a specific entry by ID
- * @param req.params
- *  - entry ID: String
- * @access // TODO: make Private (requires access token)
+ * @param req.params entry ID: String
  */
-// TODO: Add verifyAccessToken
 router.delete('/:id', deleteEntry);
 
 /**
  * @route POST /entries
  * @description Create a new entry
- * @access // TODO: make Private (requires access token)
  * @param {Object} req - The requst object containing:
  *  - body: String
  *  - userId: String
  */
-// TODO: Add verifyAccessToken
 router.post('/', addEntry);
 
 /**
  * @route PATCH /entries/:id
  * @description Update a specific entry by ID
- * @param req.params
- *  - entry ID: String
- * @access // TODO: make Private (requires access token)
+ * @param req.params entry ID: String
  */
-// TODO: Add verifyAccessToken
 router.patch('/:id', updateEntry);
 
 module.exports = router;
