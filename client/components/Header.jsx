83afe5c77c71c742c9c4286cc2fa69e6791e4dd5
Gayle Martin <hello.gmartin@gmail.com>
2023-12-04 18:53:53 -0600

    Redirect user to /home or /login on header title click and made minor CSS improvements

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -2,6 +2,7 @@
 import React, { useState } from 'react';
 import { useLogout } from '../hooks/useLogout';
 import { useAuthContext } from '../hooks/useAuthContext';
+import { useNavigate } from 'react-router-dom';
 
 // Import header styles
 import '../scss/header.scss';
@@ -24,8 +25,11 @@ function Header() {
   // useAuthContext hook is used to retrieve user information for authentication.
   const { user } = useAuthContext();
 
+  // useHistory hook is used to redirect the user to either the homepage or login page when they click the title
+  const navigate = useNavigate();
+
   // handleClick function manages logout button clicks and refresh tokens in local storage.
-  const handleClick = () => {
+  const handleLogoutBtnClick = () => {
     logout(user.userId);
   };
 
@@ -35,6 +39,17 @@ function Header() {
   // toggleSidebar function toggles the sidebar open and closed.
   const toggleSidebar = () => {
     setSidebarOpen(!sidebarOpen);
+  };
+
+  //
+  const handleTitleClick = () => {
+    if (user) {
+      // Redirect to the homepage if the user is logged in
+      navigate('/');
+    } else {
+      // Redirect to the login page if the user is not logged in
+      navigate('/login');
+    }
   };
 
   /**
@@ -51,9 +66,9 @@ function Header() {
         {/* Hamburger menu icon toggles the sidebar open and closed when clicked. */}
         <img className='hamburger' onClick={toggleSidebar} src={hamburger} alt='hamburger-menu' />
         {/* Application title */}
-        <h1>quil</h1>
+        <h1 onClick={handleTitleClick}>quil</h1>
         {/* Logout button is displayed if a user is logged in and logs the user out when clicked. */}
-        <div className='logout'>{user && <button onClick={handleClick}>Log Out</button>}</div>
+        <div className='logout'>{user && <button onClick={handleLogoutBtnClick}>Log Out</button>}</div>
       </div>
       {/* Sidebar component for internal navigation links to other pages. */}
       <Sidebar isOpen={sidebarOpen} toggleSidebar={toggleSidebar} />
