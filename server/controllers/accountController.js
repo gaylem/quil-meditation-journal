660c7639baac0f336319c267e52b830ab9ccd8cf
Gayle Martin <hello.gmartin@gmail.com>
2023-12-16 21:45:19 -0600

    Build accountControllers

--- server/controllers/accountController.js
+++ server/controllers/accountController.js
@@ -16,6 +16,7 @@ import { Entry } from '../db/models.js';
 import bcrypt from 'bcryptjs';
 import { stringify } from 'csv-stringify';
 import {decrypt } from '../utils/encrypt-decrypt-utils.js'
+import { verifyPassword } from '../utils/credentials.utils.js';
 
 
 // accountController object that contains the methods below
@@ -29,12 +30,18 @@ const accountController = {};
  */
 accountController.downloadEntries = async (req, res) => {
   const userId = req.params.userId
-  console.log('userId: ', userId);
-  
+  const password = req.body.enterPassword;
+  console.log('password: ', password);
   try {
-    const allEntries = await Entry.find({userId}).select('-_id body createdAt updatedAt iv');
-    console.log('allEntries: ', allEntries);
-
+    // Authenticate user
+    const passwordIsValid = await verifyPassword(password, userId)
+    console.log('passwordIsValid: ', passwordIsValid);
+    if (!passwordIsValid) {
+      return res.status(400).send();
+    }
+    // Pull all user entries from database
+    const allEntries = await Entry.find({ userId }).select('-_id body createdAt updatedAt iv');
+    // Format and decrypt entries
     const formattedEntries = allEntries.map(entry => {
       entry.toObject();
       const decryptedBody = decrypt(entry.body, entry.iv);
@@ -76,10 +83,14 @@ accountController.downloadEntries = async (req, res) => {
     // Send the CSV string as the response
     res.status(200).send(csvString);
   } catch (error) {
-    console.error('Error generating CSV:', error);
-    res.status(500).send('Internal Server Error');
+    console.error('accountController.downloadEntries Error:', error);
+    return res.status(500).json({
+      log: 'accountController.downloadEntries Error',
+      status: 500,
+      message: 'An error occurred while processing the request.',
+    });
   }
-};
+}
 
 /**
  * @route PUT /api/users/:userId
@@ -89,32 +100,37 @@ accountController.downloadEntries = async (req, res) => {
  * @returns {Object} - JSON with updated username and 200 status, or error message
  */
 accountController.updateUsername = async (req, res) => {
+  
+  console.log("test");
   try {
     // Extract the userId, username, and password from params and body
     const { userId } = req.params;
-    const { newUsername } = req.body;
+    console.log('userId: ', userId);
+    const { newUsername, password } = req.body;
+    // Authenticate user
+    verifyPassword(password, userId)
     // Check if newUsername already exists
-    const user = await User.findOne({ username: newUsername });
+    const user = await User.findOne({ _id: userId });
     // Throw error if username already exists
-    if (user) {
+    if (!user) {
       return res.status(404).json({
-        log: 'userController.updateUsername: Username already exists',
+        log: 'userController.updateUsername: User does not exist',
         status: 404,
-        message: 'Username not available.',
+        message: 'Something went wrong, please try again.',
       });
     }
     // Update the user data
     const updatedUser = await User.findOneAndUpdate({ _id: userId }, { $set: { username: newUsername } });
     // If nothing is returned, throw an error
-    if (result.matchedCount === 0) {
+    if (updatedUser.matchedCount === 0) {
       return {
         log: 'User not found.',
         status: 404,
         message: { error: 'User not found' },
       };
     }
-    res.locals.user = updatedUser;
-    return res.status(200).json(res.locals.user);
+
+    return res.status(200).json(newUsername);
   } catch (error) {
     console.error('userController.updateUsername Error:', error);
     return {
@@ -134,13 +150,25 @@ accountController.updateUsername = async (req, res) => {
  */
 accountController.updateEmail = async (req, res) => {
   try {
-    // Extract the userId, username, and password from params and body
+   // Extract the userId, username, and password from params and body
     const { userId } = req.params;
-    const { newEmail } = req.body;
+    const { newEmail, password } = req.body;
+    // Authenticate user
+    verifyPassword(password, userId)
+    // Check if newUsername already exists
+    const user = await User.findOne({ _id: userId });
+    // Throw error if username already exists
+    if (!user) {
+      return res.status(404).json({
+        log: 'userController.updateUsername: User does not exist',
+        status: 404,
+        message: 'Something went wrong, please try again.',
+      });
+    }
     // Update the user data
     const updatedUser = await User.findOneAndUpdate({ _id: userId }, { $set: { email: newEmail } });
     // If nothing is returned, throw an error
-    if (result.matchedCount === 0) {
+    if (updatedUser.matchedCount === 0) {
       return {
         log: 'User not found.',
         status: 404,
@@ -170,39 +198,35 @@ accountController.updatePassword = async (req, res) => {
   try {
     // Extract the userId, username, and password from params and body
     const { userId } = req.params;
-    const { oldPassword, newPassword, confirmedNewPassword } = req.body;
+    const { currentPassword, newPassword, confirmNewPassword } = req.body;
+
     // Check if newPassword & confirmedNewPassword match
-    if (newPassword !== confirmedNewPassword) {
+    if (newPassword !== confirmNewPassword) {
       return res.status(401).json({
-        log: 'accountController: newPassword and confirmedPassword do not match',
+        log: 'accountController: newPassword and confirmNewPassword do not match',
         status: 401,
         message: 'Passwords do not match.',
       });
     }
     // Find user by userId
     const user = await User.findOne({ _id: userId });
-    // Throw error if username is incorrect
+    // Throw error if no user
     if (!user) {
       return res.status(404).json({
-        log: 'userController.loginUser: No user found',
+        log: 'accountController.update: No user found',
         status: 404,
         message: 'Username or password are incorrect.',
       });
     }
     // Confirm old password matches user password
-    const match = await bcrypt.compare(oldPassword, user.password);
-    // Throw error if password is incorrect
-    if (!match) {
-      return res.status(404).json({
-        log: 'accountController.updatePassword: Incorrect password',
-        status: 404,
-        message: 'Something went wrong, please try again.',
-      });
-    }
+    verifyPassword(currentPassword, userId);
     // Encrypt the password before updating the user
-    const encryptedPassword = bcrypt.hash(newPassword, process.env.SALT_WORK_FACTOR);
+    const saltRounds = Number(process.env.SALT_WORK_FACTOR);
+    const salt = await bcrypt.genSalt(saltRounds);
+    const hash = await bcrypt.hash(newPassword, salt);
     // Update the user data
-    const updatedUser = await User.findOneAndUpdate({ _id: userId }, { $set: { password: encryptedPassword } });
+    const updatedUser = await User.findOneAndUpdate({ _id: userId }, { $set: { password: hash } });
+    console.log('updatedUser: ', updatedUser);
     // If nothing is returned, throw an error
     if (updatedUser.matchedCount === 0) {
       return {
@@ -211,7 +235,7 @@ accountController.updatePassword = async (req, res) => {
         message: { error: 'User not found' },
       };
     }
-    return res.status(200).redirect('/login');
+    return res.status(204).send();
   } catch (err) {
     console.error('userController.updateUser Error:', err);
     return {
@@ -231,18 +255,22 @@ accountController.updatePassword = async (req, res) => {
 accountController.deleteAccount = async (req, res, next) => {
   try {
     const { userId } = req.params;
-    const userDeleteResult = await User.findOneAndDelete({ _id: userId });
-    res.locals.deletedUser = userDeleteResult;
-    if (userDeleteResult === null) {
+    const deletedUser= await User.findOneAndDelete({ _id: userId });
+    if (deletedUser === null) {
       return next({
         log: 'User could not be deleted.',
         status: 404,
         message: { error: 'An error occurred' },
       });
     }
-    return res.status(200).json(res.locals.deletedUser);
-  } catch (err) {
-    console.error('userController.deleteUser Error:', err);
+
+    res.clearCookie('refreshToken');
+    return res.status(200).json({
+      message: 'User account deleted successfully',
+      redirect: '/signup', // Adjust the redirect URL as needed
+    });
+  } catch (error) {
+    console.error('userController.deleteUser Error:', error);
     return next({
       log: 'userController.deleteUser Error',
       message: { error: 'An error occurred' },
