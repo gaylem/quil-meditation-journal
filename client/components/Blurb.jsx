b4551e9c593764026afb91eae96881b628210f43
Gayle Martin <hello.gmartin@gmail.com>
2023-12-16 21:52:44 -0600

    Correct formatting

--- client/components/Blurb.jsx
+++ client/components/Blurb.jsx
@@ -9,16 +9,16 @@ import React from 'react';
  */
 function Blurb() {
   return (
-      <div className='about'>
-        {/* First paragraph of about section */}
-        <div className='paragraph'>
-          <p>quil is inspired by the words &quot;tranquil&quot; and &quot;quill,&quot; as in the classic feather pen. Sign up or log in to discover your own dedicated space, where you can chronicle each step of your meditation journey.</p>
-        </div>
-        {/* Second paragraph of about section */}
-        <div>
-          <p>Every entry you record in your account is encrypted to ensure absolute privacy and confidentiality. Put your mind at ease and your worries aside, and immerse yourself fully in the transformative experience of meditation. We hope we can help your innermost thoughts find a safe harbor amid the turbulent waves of life.</p>
-        </div>
+    <div className='about'>
+      {/* First paragraph of about section */}
+      <div className='paragraph'>
+        <p>quil is inspired by the words &quot;tranquil&quot; and &quot;quill,&quot; as in the classic feather pen. Sign up or log in to discover your own dedicated space, where you can chronicle each step of your meditation journey.</p>
       </div>
+      {/* Second paragraph of about section */}
+      <div>
+        <p>Every entry you record in your account is encrypted to ensure absolute privacy and confidentiality. Put your mind at ease and your worries aside, and immerse yourself fully in the transformative experience of meditation. We hope we can help your innermost thoughts find a safe harbor amid the turbulent waves of life.</p>
+      </div>
+    </div>
   );
 }
 
--- server/utils/token.utils.js
+++ server/utils/token.utils.js
@@ -93,4 +93,3 @@ export const refreshTokens = async refreshToken => {
     };
   }
 };
-
