6923552fd40b4c374de91a048e2b3e5d2a5d91f5
Gayle Martin <hello.gmartin@gmail.com>
2023-12-20 19:59:34 -0600

    Add labels to contact form

--- client/components/ContactForm.jsx
+++ client/components/ContactForm.jsx
@@ -11,15 +11,15 @@ function ContactForm() {
   return (
     <div className='contactFormContainer'>
       <form action={process.env.REACT_APP_FORM_ENDPOINT} method='POST' className='form'>
-        <label for='name'>Your Name:</label>
+        <label htmlFor='name'>Your Name:</label>
         <input type='text' name='name' id='name' placeholder='ex: Jane Doe' className='name' />
         <div className='visually-hidden'>
           <label htmlFor='hidden'>Leave this field empty:</label>
           <input type='text' id='hidden' name='_gotcha' />
         </div>
-        <label for='email'>Your Email:</label>
+        <label htmlFor='email'>Your Email:</label>
         <input type='text' name='email' id='email' placeholder='janedoe@example.com' className='email' />
-        <label for='message'>Message:</label>
+        <label htmlFor='message'>Message:</label>
         <textarea type='text' name='message' id='message' placeholder="What's on your mind?" rows='10' className='message' />
         <button type='submit' className='submitBtn'>
           Send
