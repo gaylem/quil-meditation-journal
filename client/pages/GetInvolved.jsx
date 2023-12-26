4c698656682a238bdd011540a14e7baf69b45233
Gayle Martin <hello.gmartin@gmail.com>
2023-12-26 15:34:42 -0600

    Remove pageContent class and make minor styling adjustments

--- client/pages/GetInvolved.jsx
+++ client/pages/GetInvolved.jsx
@@ -15,23 +15,23 @@ import ContactForm from '../components/ContactForm.jsx';
 const About = () => {
   return (
     <div className='pageContainer'>
-        <h2>Get Involved!</h2>
-        <p>
-          Because quil is an open-source product, <span className='purple'>we&apos;re contribution friendly!</span> If you&apos;re a software developer, and you&apos;d like to help make quil the best it can be, check out our{' '}
-          <span className='purple'>
-            <a href='https://github.com/gaylem/quil-meditation-journal'>GitHub</a>
-          </span>{' '}
-          for more information on how you can participate.
-        </p>
-        <br />
-        <h2>Questions?</h2>
-        <p>
-          If you encounter any issues with our app, have any questions, or just want to connect, please reach out through the form below. <span className='purple'>We&apos;d love to hear from you!</span>
-        </p>
-        <br />
-        <h2>Contact Us!</h2>
-        <ContactForm />
-      </div>
+      <h2>Get Involved!</h2>
+      <p>
+        Because quil is an open-source product, <span className='purple'>we&apos;re contribution friendly!</span> If you&apos;re a software developer, and you&apos;d like to help make quil the best it can be, check out our{' '}
+        <span className='purple'>
+          <a href='https://github.com/gaylem/quil-meditation-journal'>GitHub</a>
+        </span>{' '}
+        for more information on how you can participate.
+      </p>
+      <br />
+      <h2>Questions?</h2>
+      <p>
+        If you encounter any issues with our app, have any questions, or just want to connect, please reach out through the form below. <span className='purple'>We&apos;d love to hear from you!</span>
+      </p>
+      <br />
+      <h2>Contact Us!</h2>
+      <ContactForm />
+    </div>
   );
 };
 
