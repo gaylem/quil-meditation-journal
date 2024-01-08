9fe124f6ad6e319619f1a4d96743f05d06d8e6af
Gayle Martin <hello.gmartin@gmail.com>
2024-01-07 19:18:48 -0600

    Revert axiosConfig

--- client/axiosConfig.js
+++ client/axiosConfig.js
@@ -2,25 +2,21 @@
 
 import axios from 'axios';
 
+// Define base URLs for different environments
 const baseURLs = {
   development: 'http://localhost:4000',
   staging: 'https://quil-staging-97e232bad7d0.herokuapp.com',
   production: 'https://quil-prod-b3e044c49835.herokuapp.com',
 };
 
+// Determine the environment based on NODE_ENV or default to 'development'
 const environment = process.env.NODE_ENV;
 
-const axiosInstance = axios.create({
+// Create an instance of Axios with a custom configuration
+const instance = axios.create({
+  // Set the base URL for requests
   baseURL: baseURLs[environment],
+  withCredentials: true,
 });
 
-axiosInstance.defaults.withCredentials = true;
-
-// Add an interceptor to dynamically set the base URL before each request
-axiosInstance.interceptors.request.use((config) => {
-  // Set the base URL based on the environment
-  config.baseURL = baseURLs[process.env.NODE_ENV];
-  return config;
-});
-
-export default axiosInstance;
+export default instance;
