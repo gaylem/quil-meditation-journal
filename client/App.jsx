8c661e8f565c587bc13027672a35d6b944afab95
Gayle Martin <hello.gmartin@gmail.com>
2024-01-12 11:07:53 -0600

    Add promise back into useEffect hook

--- client/App.jsx
+++ client/App.jsx
@@ -33,11 +33,16 @@ function App() {
   const [showFooter, setShowFooter] = useState(false);
 
   useEffect(() => {
-    const delayFooter = setTimeout(() => {
-      setShowFooter(true);
-    }, 6000); // Adjust the delay time (in milliseconds) as needed
+    const loadFooter = async () => {
+      await Promise.all([
+        import(/* webpackChunkName: "footer" */ './components/Footer.jsx'),
+        new Promise(resolve => setTimeout(resolve, 5000)), // Adjust the delay time as needed
+      ]);
 
-    return () => clearTimeout(delayFooter); // Cleanup the timeout on component unmount
+      setShowFooter(true);
+    };
+
+    loadFooter();
   }, []);
 
   return (
