93a7c14d1efb127308ce5eb77b04fb497be4ad8a
Gayle Martin <hello.gmartin@gmail.com>
2024-01-22 16:34:25 -0600

    Add clientIP and console log to loginLimiter function

--- server/middlewares/rateLimiter.js
+++ server/middlewares/rateLimiter.js
@@ -15,15 +15,18 @@ export const signupLimiter = rateLimit({
     res.status(response.status).json(response);
   },
 });
-
 export const loginLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 5, // Allow 5 requests per windowMs
   handler: (req, res) => {
+    const clientIP = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+    console.log(`Rate limit exceeded for IP address: ${clientIP}`);
+
     const response = {
       status: 429, // HTTP status code for "Too Many Requests"
       message: 'Too many requests, please try again later.',
     };
+
     res.status(response.status).json(response);
   },
 });
