e1a7407428153329ccb7633284d968e621e20b39
Gayle Martin <hello.gmartin@gmail.com>
2024-01-23 09:03:51 -0600

    Add handleSliderChange function so that volume change works on mobile devices

--- client/components/VolumeSlider.jsx
+++ client/components/VolumeSlider.jsx
@@ -14,11 +14,20 @@ const VolumeSlider = ({ audioElement, volume, setVolume }) => {
     audioElement.volume = newValue / 100;
   };
 
+  const handleSliderChange = () => {
+    // When the slider is clicked or touched, unmute and play audio
+    audioElement.muted = false;
+    audioElement.play().catch(error => {
+      // Handle autoplay error, if needed
+      console.error('Autoplay error:', error);
+    })
+  };
+
   return (
     <div className='audio-slider'>
       {/* Use the StyledSlider component instead of Slider */}
       <img src='assets/images/volume-off.png' className='high' alt='Volume off icon' />
-      <StyledSlider valueLabelDisplay='auto' value={volume} onChange={handleVolumeChange} aria-label='Volume Slider' />
+      <StyledSlider valueLabelDisplay='auto' value={volume} onChange={handleVolumeChange} onMouseDown={handleSliderChange} onTouchStart={handleSliderChange} aria-label='Volume Slider' />
       <img src='assets/images/volume-high.png' className='low' alt='Volume high icon' />
     </div>
   );
