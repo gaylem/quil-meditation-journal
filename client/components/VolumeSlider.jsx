f66454adefeacdacaa44e3ed5395857865254961
Gayle Martin <hello.gmartin@gmail.com>
2024-01-23 09:31:27 -0600

    Update handler functions for touch

--- client/components/VolumeSlider.jsx
+++ client/components/VolumeSlider.jsx
@@ -14,20 +14,36 @@ const VolumeSlider = ({ audioElement, volume, setVolume }) => {
     audioElement.volume = newValue / 100;
   };
 
-  const handleSliderChange = () => {
-    // When the slider is clicked or touched, unmute and play audio
-    audioElement.muted = false;
-    audioElement.play().catch(error => {
-      // Handle autoplay error, if needed
-      console.error('Autoplay error:', error);
-    })
+  const handleMouseDown = () => {
+    // When the slider is clicked, update the volume
+    audioElement.muted = false; // Unmute if needed
+    const newVolume = volume; // Use the existing volume
+    audioElement.volume = newVolume / 100;
+  };
+
+  const handleTouchStart = event => {
+    // Log touch start event
+    console.log('Touch started');
+
+    // Calculate the percentage of the slider where the touch occurred
+    const touchPercentage = (event.touches[0].clientX - event.currentTarget.getBoundingClientRect().left) / event.currentTarget.clientWidth;
+
+    // Calculate the new volume based on the touch percentage
+    const newVolume = Math.min(100, Math.max(0, Math.round(touchPercentage * 100)));
+
+    // Update the volume state and audio element
+    setVolume(newVolume);
+    audioElement.volume = newVolume / 100;
+
+    // Log the new volume
+    console.log('New volume:', newVolume);
   };
 
   return (
     <div className='audio-slider'>
       {/* Use the StyledSlider component instead of Slider */}
       <img src='assets/images/volume-off.png' className='high' alt='Volume off icon' />
-      <StyledSlider valueLabelDisplay='auto' value={volume} onChange={handleVolumeChange} onMouseDown={handleSliderChange} onTouchStart={handleSliderChange} aria-label='Volume Slider' />
+      <StyledSlider valueLabelDisplay='auto' value={volume} onChange={handleVolumeChange} onMouseDown={handleMouseDown} onTouchStart={handleTouchStart} aria-label='Volume Slider' />
       <img src='assets/images/volume-high.png' className='low' alt='Volume high icon' />
     </div>
   );
