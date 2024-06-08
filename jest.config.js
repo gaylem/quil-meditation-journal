799ad6226f63a239c92d0b51a42b882ba690cd84
Gayle Martin <hello.gmartin@gmail.com>
2024-06-08 13:51:26 -0500

    Update jest config file

--- jest.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  testEnvironment: 'node', // or 'jsdom' for browser-like environment
-  testMatch: ['**/__tests__/**/*.js?(x)', '**/?(*.)+(spec|test).js?(x)'],
-  coverageReporters: ['lcov', 'text-summary', 'html'],
-};
